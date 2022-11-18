4030f6f911804e6b7d2c89d85ab0cce4328b4443
Totenkaf <uscov.1998@mail.ru>
2022-11-19 02:55:26 +0300

    refactoring

--- program_options_lib/program_options.cpp
+++ program_options_lib/program_options.cpp
@@ -2,124 +2,125 @@
 
 #include "program_options.hpp"
 
-
 void ArgumentParser::fill_by_default_args() {
-    for (const auto& [key, value] : argtemplate) {
-        fill_args(key, value[0], value[1]);
-    }
+  for (const auto& [key, value] : argtemplate) {
+    fill_args(key, value[0], value[1]);
+  }
 }
 
-auto ArgumentParser::parse_parametrize_arg(std::string arg) {
-    std::string delimiter = "=";
-    std::string token = arg.substr(0, arg.find(delimiter));
-    arg.erase(0, arg.find(delimiter) + delimiter.length());
-    return std::make_pair(token, arg);
+auto ArgumentParser::parse_parametrize_arg(std::string arg)
+    -> std::pair<std::string, std::string> {
+  std::string delimiter = "=";
+  std::string token = arg.substr(0, arg.find(delimiter));
+  arg.erase(0, arg.find(delimiter) + delimiter.length());
+  return std::make_pair(token, arg);
 }
 
-
-void ArgumentParser::fill_args(std::string arg, std::string type, std::string value) {
-    try {
-        if (type == "int") {
-            int_args[arg] = stoi(value);
-        } else if (type == "float") {
-            float_args[arg] = stof(value);
-        } else if (type == "double") {
-            double_args[arg] = stod(value);
-        } else {
-            string_args[arg] = value;
-        }
-    } catch(const std::exception& e) {
-        throw std::invalid_argument("value of arg <<" + arg + ">> is not convertable\n" +
-                                    "template type is <<" + type + ">>\n" +
-                                    "gave <<" + value + ">>");
+void ArgumentParser::fill_args(const std::string& arg, const std::string& type,
+                               const std::string& value) {
+  try {
+    if (type == "int") {
+      int_args[arg] = stoi(value);
+    } else if (type == "float") {
+      float_args[arg] = stof(value);
+    } else if (type == "double") {
+      double_args[arg] = stod(value);
+    } else {
+      string_args[arg] = value;
     }
+  } catch (const std::exception& e) {
+    throw std::invalid_argument(
+        "value of arg <<" + arg + ">> is not convertable\n" +
+        "template type is <<" + type + ">>\n" + "gave <<" + value + ">>");
+  }
 }
 
+void ::ArgumentParser::parse_args(int argc, char const* argv[]) {
+  if (argc < 2) {
+    std::cout << "too few arguments" << std::endl;
+    return;
+  }
+  try {
+    fill_by_default_args();
 
-
-void::ArgumentParser::parse_args(int argc, char const *argv[]) {
-    if (argc < 2) {
-        std::cout << "too few arguments" << std::endl;
-        return;
+    for (int i = 1; i < argc; ++i) {
+      auto arg = std::string(argv[i]);
+      if (arg.starts_with("--")) {
+        auto parsed_arg = parse_parametrize_arg(arg);
+        auto type = argtemplate.at(parsed_arg.first)[0];
+        fill_args(parsed_arg.first, type, parsed_arg.second);
+      } else {
+        auto type = argtemplate.at(arg)[0];
+        fill_args(arg, type, argv[++i]);
+      }
     }
-    try {
-        fill_by_default_args();
-
-        for (int i = 1; i < argc; ++i) {
-            auto arg = std::string(argv[i]);
-            if (arg.starts_with("--")) {
-                auto parsed_arg = parse_parametrize_arg(arg);
-                auto type = argtemplate.at(parsed_arg.first)[0];
-                fill_args(parsed_arg.first, type, parsed_arg.second);
-            } else {
-                auto type = argtemplate.at(arg)[0];
-                fill_args(arg, type, argv[++i]);
-            }
-        }
-    } catch(const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return;
-    }
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return;
+  }
 }
 
-std::string ArgumentParser::to_str(const char* c) {
-    return std::string(c);
-}
+std::string ArgumentParser::to_str(const char* c) { return std::string(c); }
 
-
-void ArgumentParser::make_args_template(std::vector<std::string> keys, std::string type, std::string value) {
-    for (const auto& key : keys) {
-        if (!key.empty()) {
-            auto template_pair = std::make_pair(key, std::vector<std::string>{type, value});
-            if(!argtemplate.insert(template_pair).second) {
-                throw std::invalid_argument("key already exists");
-            }
-        }
+void ArgumentParser::make_args_template(const std::vector<std::string>& keys,
+                                        const std::string& type,
+                                        const std::string& value) {
+  for (const auto& key : keys) {
+    if (!key.empty()) {
+      auto template_pair =
+          std::make_pair(key, std::vector<std::string>{type, value});
+      if (!argtemplate.insert(template_pair).second) {
+        throw std::invalid_argument("key already exists");
+      }
     }
+  }
 }
 
 void ArgumentParser::print_argument_map() {
-    for (const auto& [key, value] : argtemplate) {
-        std::cout << key << ", " << value[0] << ", " << value[1] << std::endl;
-    }
+  for (const auto& [key, value] : argtemplate) {
+    std::cout << key << ", " << value[0] << ", " << value[1] << std::endl;
+  }
 }
 
-bool ArgumentParser::check_params(std::string first, const std::vector<std::string>& params) const {
-    static const size_t MAX_SHORT_KEY_SIZE = 3;
+auto ArgumentParser::check_params(const std::string& first,
+                                  const std::vector<std::string>& params) const
+    -> bool {
+  static const size_t MAX_SHORT_KEY_SIZE = 3;
 
-    auto short_prefix = std::string_view("-");
-    auto first_string = std::string(first);
-    std::set<std::string> valid_values = {"string", "int", "float", "double"};
-    if (!first_string.starts_with(short_prefix) || first_string.size() > MAX_SHORT_KEY_SIZE) {
-        std::cout << "it is not short key" << std::endl;
-        return false;
-    }
+  auto short_prefix = std::string_view("-");
+  auto first_string = std::string(first);
+  std::set<std::string> valid_values = {"string", "int", "float", "double"};
+  if (!first_string.starts_with(short_prefix) ||
+      first_string.size() > MAX_SHORT_KEY_SIZE) {
+    std::cout << "it is not short key" << std::endl;
+    return false;
+  }
 
-    auto long_prefix = std::string_view("--");
-    switch (params.size()) {
+  auto long_prefix = std::string_view("--");
+  switch (params.size()) {
     case 3:
-        if (!params[0].starts_with(long_prefix)) {
-            std::cout << "it is not long key" << std::endl;
-            return false;
-        } else if (valid_values.find(params[1]) == valid_values.end()) {
-            std::cout << "wrong type value" << std::endl;
-            return false;
-        }
-        break;
-    case 2:
-        if (valid_values.find(params[0]) == valid_values.end()) {  
-            std::cout << "wrong type value" << std::endl;
-            return false;
-        }
-        break;
-    default:
-        std::cout << "bad number of arguments" << std::endl;
+      if (!params[0].starts_with(long_prefix)) {
+        std::cout << "it is not long key" << std::endl;
         return false;
-    }
-    return true;
+      } else if (valid_values.find(params[1]) == valid_values.end()) {
+        std::cout << "wrong type value" << std::endl;
+        return false;
+      }
+      break;
+    case 2:
+      if (valid_values.find(params[0]) == valid_values.end()) {
+        std::cout << "wrong type value" << std::endl;
+        return false;
+      }
+      break;
+    default:
+      std::cout << "bad number of arguments" << std::endl;
+      return false;
+  }
+  return true;
 }
 
-
-std::map<std::string, std::vector<std::string>> ArgumentParser::get_default_template() const {
-    return argtemplate;
+auto ArgumentParser::get_default_template() const
+    -> std::map<std::string, std::vector<std::string>> {
+  return argtemplate;
 }
--- program_options_lib/program_options.hpp
+++ program_options_lib/program_options.hpp
@@ -1,91 +1,94 @@
 // Copyright 2022 by Artem Ustsov
 
 #pragma once
-#include <iostream>
-#include <vector>
-#include <map>
 #include <algorithm>
+#include <iostream>
+#include <map>
 #include <set>
-
+#include <vector>
 
 class ArgumentParser {
-public:
-    explicit ArgumentParser(std::string description_)
-    : description(description_)
-    {}
+ public:
+  explicit ArgumentParser(std::string description_)
+      : description(std::move(description_)) {}
 
-    template <typename... Params> 
-    void add_argument(const char* first, const Params&... params);
+  template <typename... Params>
+  void add_argument(const char* first, const Params&... params);
 
-    void print_argument_map();
-    void parse_args(int argc, char const *argv[]);
+  void print_argument_map();
+  void parse_args(int argc, char const* argv[]);
 
-    std::map<std::string, std::vector<std::string>> get_default_template() const;
+  [[nodiscard]] auto get_default_template() const
+      -> std::map<std::string, std::vector<std::string>>;
 
-    std::string get_description() const {
-        return description;
-    }
+  [[nodiscard]] auto get_description() const -> std::string {
+    return description;
+  }
 
-    std::map<std::string, int> int_args;
-    std::map<std::string, float> float_args;
-    std::map<std::string, double> double_args;
-    std::map<std::string, std::string> string_args;
+  std::map<std::string, int> int_args;
+  std::map<std::string, float> float_args;
+  std::map<std::string, double> double_args;
+  std::map<std::string, std::string> string_args;
 
-private:
-    std::string description;
-    std::map<std::string, std::vector<std::string>> argtemplate;
+ private:
+  std::string description;
+  std::map<std::string, std::vector<std::string>> argtemplate;
 
-    void make_args_template(std::vector<std::string> keys, std::string type, std::string value);
-    bool check_params(std::string first, const std::vector<std::string>& params) const;
+  void make_args_template(const std::vector<std::string>& keys,
+                          const std::string& type, const std::string& value);
+  [[nodiscard]] auto check_params(const std::string& first,
+                                  const std::vector<std::string>& params) const
+      -> bool;
 
-    void fill_by_default_args();
-    void fill_args(std::string arg, std::string type, std::string value);
-    auto parse_parametrize_arg(std::string);
-    
-    template <typename ValueType>
-    std::string to_str(const ValueType &val);
+  void fill_by_default_args();
+  void fill_args(const std::string& arg, const std::string& type,
+                 const std::string& value);
+  static auto parse_parametrize_arg(std::string arg)
+      -> std::pair<std::string, std::string>;
 
-    std::string to_str(const char* c);
+  template <typename ValueType>
+  auto to_str(const ValueType& val) -> std::string;
+
+  auto to_str(const char* c) -> std::string;
 };
 
-template <typename... Params> 
+template <typename... Params>
 void ArgumentParser::add_argument(const char* first, const Params&... params) {
-    static const size_t MAX_ARGUMENTS = 3;
+  static const size_t MAX_ARGUMENTS = 3;
 
-    if (sizeof ...(params) > MAX_ARGUMENTS) {
-        std::cout << "too many arguments in template" << std::endl;
-        return;
-    }
-    
-    std::vector<std::string> params_vector {to_str(params)...};
-    std::string short_key;
-    std::string long_key;
-    std::string type;
-    std::string value;
+  if (sizeof...(params) > MAX_ARGUMENTS) {
+    std::cout << "too many arguments in template" << std::endl;
+    return;
+  }
 
-    if (check_params(std::string(first), params_vector)) {
-        short_key = std::string(first);
-        if (params_vector.size() == 3) {
-            long_key = params_vector[0];
-            type = params_vector[1];
-            value = params_vector.back();
-        }
-        else if (params_vector.size() == 2) {
-            type = params_vector[0];
-            value = params_vector.back();
-        }
-        try {
-            make_args_template(std::vector<std::string> {short_key, long_key}, type, value);
-        }
-        catch(const std::exception& e) {
-            std::cerr << e.what() << '\n';
-        }
-    } else {
-        std::cout << "bad check" << std::endl;
+  std::vector<std::string> params_vector{to_str(params)...};
+  std::string short_key;
+  std::string long_key;
+  std::string type;
+  std::string value;
+
+  if (check_params(std::string(first), params_vector)) {
+    short_key = std::string(first);
+    if (params_vector.size() == 3) {
+      long_key = params_vector[0];
+      type = params_vector[1];
+      value = params_vector.back();
+    } else if (params_vector.size() == 2) {
+      type = params_vector[0];
+      value = params_vector.back();
     }
+    try {
+      make_args_template(std::vector<std::string>{short_key, long_key}, type,
+                         value);
+    } catch (const std::exception& e) {
+      std::cerr << e.what() << '\n';
+    }
+  } else {
+    std::cout << "bad check" << std::endl;
+  }
 }
 
 template <typename T>
-std::string ArgumentParser::to_str(const T &val) {
-    return std::to_string(val);
+auto ArgumentParser::to_str(const T& val) -> std::string {
+  return std::to_string(val);
 }
